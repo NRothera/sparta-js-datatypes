b31a6a1b6c058e4b0709f55f1fe724a5887ac4fe
NRothera <31472463+NRothera@users.noreply.github.com>
2017-10-30 12:05:12 +0000

    lots of equations and variables

---
+++ main.js
@@ -0,0 +1,167 @@
+// var a = "this is a string";
+//
+// var b ="this is another string"
+//
+// var array = [a,b];
+// console.log(a);
+//
+// var combined_string = a+ " " + b
+//
+// console.log(a.indexOf('h'))
+//
+//
+// console.log(a.substr(2,4))
+//
+// console.log(a.substr(2,4))
+
+
+// var myNumber = 5;
+// var myDecimal = 5.5;
+// var myExponential = 123e5;
+//
+// console.log(myNumber+3);
+//
+// console.log(isFinite(myNumber))
+
+
+// var myBoolean =false || true;
+//
+// console.log(myBoolean)
+//
+// var myArray = [24,12,53, "hello", "goodbye", "whaduuupppp"]
+//
+// console.log(myArray)
+//
+// var join = ["onething", "something else"]
+
+// var object = {
+//   one:'this is a string',
+//   two: ["this is an array"],
+//   three: 5,
+//   four: {
+//     five: "this is a nested string"
+//   }
+// }
+//
+// console.log(object['two'])
+//
+// console.log(object)
+
+
+//
+
+var myString = "5";
+var myDecimalString="2.5"
+var myNumber = 3;
+
+console.log(parseInt(myString)+myNumber);
+
+console.log(parseFloat(myDecimalString)+myNumber);
+
+console.log(parseInt(myDecimalString)+myNumber);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
